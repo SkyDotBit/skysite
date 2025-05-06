21110a43ebe35b1439b4f0882e3aa480ebba720c
SkyDotBit <skydotbit@outlook.com>
2025-05-05 20:22:09 -0400

    Added mobile support

---
+++ style.js
@@ -0,0 +1,8 @@
+function ismobile() {
+    return /iPhone|iPad|iPod|Mobi|Android/i.test(navigator.userAgent);
+};
+if (ismobile()) {
+    console.log("It's mobile matey john or something");
+    const style = document.getElementById('style');
+    style.setAttribute('href', 'mobile.css');
+}
