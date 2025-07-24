00607affe0c4d199ad8fafab544313e7cd5fce29
SkyDotBit <skydotbit@outlook.com>
2025-07-23 23:16:02 -0400

    Play around with haptics :neofox_floof:

---
+++ tests/haptics/haptics.js
@@ -0,0 +1,32 @@
+/**
+ * ios-haptics v0.0.9
+ * tijn.dev
+ * @license MIT
+ */
+function haptic() {
+    try {
+        const labelEl = document.createElement('label');
+        labelEl.ariaHidden = 'true';
+        labelEl.style.display = 'none';
+        const inputEl = document.createElement('input');
+        inputEl.type = 'checkbox';
+        inputEl.setAttribute('switch', '');
+        labelEl.appendChild(inputEl);
+        document.head.appendChild(labelEl);
+        labelEl.click();
+        document.head.removeChild(labelEl);
+    }
+    catch {
+        // do nothing
+    }
+}
+;
+haptic.confirm = () => {
+    haptic();
+    setTimeout(() => haptic(), 120);
+};
+haptic.error = () => {
+    haptic();
+    setTimeout(() => haptic(), 120);
+    setTimeout(() => haptic(), 240);
+};
