173cfaa7f724f700b6bcd31960d8cd8dc4298504
SkyDotBit <skydotbit@outlook.com>
2025-07-24 00:32:16 -0400

    Attempt to add haptic feedback for iPhones :neofox_floof_reach:

--- browserextensions/firefox/clicky.js
+++ browserextensions/firefox/clicky.js
@@ -1,3 +1,41 @@
+/**
+ * ios-haptics v0.0.9
+ * tijn.dev
+ * @license MIT
+ */
+function haptic() {
+    try {
+        const labelEl = document.createElement('label');
+        labelEl.ariaHidden = 'true';
+        labelEl.style.display = 'none';
+        const inputEl = document.createElement('input');
+        inputEl.type = 'checkbox';
+        inputEl.setAttribute('switch', '');
+        labelEl.appendChild(inputEl);
+        document.head.appendChild(labelEl);
+        labelEl.click();
+        document.head.removeChild(labelEl);
+    }
+    catch {
+        // do nothing
+    }
+}
+;
+haptic.confirm = () => {
+    haptic();
+    setTimeout(() => haptic(), 120);
+};
+haptic.error = () => {
+    haptic();
+    setTimeout(() => haptic(), 120);
+    setTimeout(() => haptic(), 240);
+};
+function ismobile() {
+    return /iPhone|iPad|iPod|Mobi|Android/i.test(navigator.userAgent);
+};
+function isiphone() {
+    return /iPhone/i.test(navigator.userAgent);
+};
 document.querySelectorAll('.project').forEach(function(div) {
     div.addEventListener('click', function() {
         const url = this.getAttribute('url');
@@ -11,6 +49,9 @@ document.querySelectorAll('.smallglow').forEach(function(div) {
     });
     div.addEventListener('touchstart', function() {
         const url = this.getAttribute('url');
+        if (isiphone) {
+            haptic();
+        }
         window.location.href = url;
     });
 });
