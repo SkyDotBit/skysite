ebcf3232f6ab17c5d9840a44659769efac8bafbb
Sky.Bit <sky@skydevs.me>
2025-08-29 21:42:59 -0400

    Wow I'm really dumb :neofox_floof:

--- age.js
+++ age.js
@@ -1,5 +1,5 @@
 const birthyear = [9, 30, 2010];
-const programmingYear = [9, 30, 2024];
+const programmingYear = [11, 10, 2022];
 let age = 0;
 let programming = 0;
 const currentdate = new Date()
@@ -10,6 +10,7 @@ age = currentyear - birthyear[2];
 if (currentmonth < birthyear[1] || (currentmonth === birthyear[1] && currentday < birthyear[0])) {
     age--;
 }
+programming = currentyear - programmingYear[2];
 if (currentmonth < programmingYear[1] || (currentmonth === programmingYear[1] && currentday < programmingYear[0])) {
     programming--;
 }
@@ -17,4 +18,4 @@ console.log(age);
 const agecounter = document.getElementById("age");
 agecounter.textContent = age;
 const programmingCounter = document.getElementById("programming");
-programmingCounter.textContent = programmingCounter;
+programmingCounter.textContent = programming;
